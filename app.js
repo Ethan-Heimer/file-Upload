fbfd0fe8df8e12cf969468a9a5d5b58044601f19
Ethan-Heimer <111470266+Ethan-Heimer@users.noreply.github.com>
2023-09-15 13:55:35 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const multer = require("multer");
+const upload = multer({ dest: 'uploads/' })
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.get("/", (req, res) => {
+    res.render("display", {data: ""});
+})
+
+app.post('/upload', upload.single("file"), (req, res) => {
+    console.log(req.file);
+    res.render("display", {data: req.file.data});
+})
+
+app.listen(3000);
